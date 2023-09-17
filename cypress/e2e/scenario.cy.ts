de1178951810b085aa325f63bed913ac00f95bed
vlad <zippo585@gmail.com>
2023-09-18 00:50:29 +0500

    first tests

--- cypress/e2e/scenario.cy.ts
+++ cypress/e2e/scenario.cy.ts
@@ -2,11 +2,12 @@ describe('Scenario Tests', () => {
     beforeEach(() => {
         cy.login();
     });
-    it('should create a new scenario', () => {
-        cy.visit('/scenarios'); // Replace with your auth page path
 
-        const testScenarioName = 'Test Scenario';
-        const testScenarioDesc = 'Test Desc';
+    const testScenarioName = 'Test Scenario';
+    const testScenarioDesc = 'Test Desc';
+
+    it('should create a new scenario', () => {
+        cy.visit('/scenarios');
 
         cy.get('button').contains('Создать сценарий').click();
         cy.get('button').contains('Создать новый').click();
@@ -17,5 +18,34 @@ describe('Scenario Tests', () => {
         cy.reload();
         cy.wait(4000);
         cy.contains(testScenarioName).should('be.visible');
+    });
+
+    it("should archive new Test Scenario", () => {
+        cy.visit('/scenarios');
+        cy.wait(2000);
+        cy.contains(testScenarioName).should('be.visible');
+        cy.contains(testScenarioName).within(() => {
+            cy.getByTestId('scenarioSettingsButton').click();
+        })
+
+        cy.getByTestId('addToArchiveBtn').click();
+        cy.getByTestId('modalAddArchiveBtn').click();
+        //Появляется Notification об успешном перемещении в архив
+        cy.contains("Сценарий перемещен в архив").should('be.visible');
+        cy.contains(testScenarioName).should('not.exist');
+    });
+
+    it("should show scroll button", () => {
+        cy.visit('/scenarios');
+        cy.scrollTo(0, 500)
+        cy.getByTestId("scrollToTopBtn").should('be.visible');
+    });
+
+    it("should not show scroll button", () => {
+        cy.visit('/scenarios');
+        cy.wait(2000);
+        cy.scrollTo(0, 500)
+        cy.scrollTo(0, 0)
+        cy.getByTestId("scrollToTopBtn").should('not.be.visible');
     });
 });
