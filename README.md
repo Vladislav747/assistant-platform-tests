7158103c35df6e0bf603bee0d191cb71375eeed0
vlad <zippo585@gmail.com>
2023-09-20 10:59:25 +0500

    fix tests

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 ## Запуск проекта
- 
+-  Устанавливаем зависимости
+```npm i``` 
+- Запуск Cypress
+```npm run test:e2e ```
+- 
+
+
 ## Сущности (entities)
 cy.log(JSON.stringify(body))
--- cypress/e2e/analytics.cy.ts
+++ cypress/e2e/analytics.cy.ts
@@ -1,18 +1,33 @@
-describe('Scenario Tests', () => {
+describe("Scenario Tests", () => {
     beforeEach(() => {
         cy.login();
+        cy.intercept(
+            "GET",
+            "**/projects/1/scenarios_starts?start_block_type=start",
+            { fixture: "analytics/scenarioStarts.json" }
+        );
+        cy.intercept("POST", "**/analytics/1/conversion/problem_calls", {
+            fixture: "analytics/conversionProblemCalls.json",
+        });
+        cy.intercept("POST", "**/analytics/1/conversion/general", {
+            fixture: "analytics/conversionGeneral.json",
+        });
+        cy.intercept("GET", "**/campaigns/project_list/1?scenario_id=641", {
+            fixture: "analytics/scenarioProjectList.json",
+        });
     });
 
-    it('Аналитика правильно считывает данные из ссылки', () => {
-        cy.intercept('GET', '**/projects/1/scenarios_starts?start_block_type=start', {fixture: 'analytics/scenarioStarts.json'});
-        cy.intercept('POST', '**/analytics/1/conversion/problem_calls', {fixture: 'analytics/conversionProblemCalls.json'});
-        cy.intercept('POST', '**/analytics/1/conversion/general', {fixture: 'analytics/conversionGeneral.json'});
-        cy.intercept('GET', '**/campaigns/project_list/1?scenario_id=641', {
-            fixture: 'analytics/scenarioProjectList.json'});
-        cy.visit('total-analytics?projectId=1&scenario_id=641&date_start=1688151600000&date_end=1690743599000');
-        //@TODO проверить вывод данных в полях
+    it("Аналитика правильно считывает данные из ссылки", () => {
+        cy.visit("total-analytics");
+        // 1688151600000 === 1 July, 1690743599000=== 30 Jul
+        cy.visit(
+            "total-analytics?projectId=1&scenario_id=641&date_start=1688151600000&date_end=1690743599000"
+        );
+        //Проверить вывод данных в полях в фильтрах
+        cy.getByTestId("listbox-button").contains("Анкетирование");
+        cy.contains("Дата");
+        cy.contains("01.07.2023 00:00");
+        cy.contains("30.07.2023 23:59");
+        cy.getByTestId("listbox-button").contains("Выберите обзвон");
     });
-
-
-
-})
+});
